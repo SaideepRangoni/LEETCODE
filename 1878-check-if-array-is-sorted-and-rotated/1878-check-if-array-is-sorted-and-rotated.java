5c1f3f82b4d11c82a515d21342c0aad19a731707
SAIDEEP RANGONI <167851189+SaideepRangoni@users.noreply.github.com>
2025-08-23 18:55:54 +0530

    Time: 0 ms (100%), Space: 41.4 MB (38.74%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,30 +1,19 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n=nums.length;
-        int cnt=0;
-
-        // Iterate through each possible rotation of the array
-        for(int i=0; i<n; i++){
-            // Reset the counter for each rotation
-            cnt=0;
-
-            // Check if the array is non-decreasing from index i
-            for(int j=i+1; j<i+n; j++){
-                // Compare the current element with the previous element in the rotated array
-                if(nums[(j-1)%n]<=nums[j%n]){
-                    cnt++; // Increment the counter if the order is maintained
-                }
-                else{
-                    break; // Break the loop if the order is not maintained                    
-                }
-            }
-
-            // If the array is non-decreasing for the entire rotation, return true            
-            if(cnt+1==n){
-                return true;
+        int count=0;
+        int k=nums.length;
+        for (int i=0;i<k;i++){
+            if(nums[i]>nums[(i+1)%k]){
+                count++;
             }
         }
+        if (count>1){
+            return false;
 
-        return false;
+        }
+        else{
+            return true;
+        }
+        
     }
 }
